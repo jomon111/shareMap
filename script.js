8d5d4fcf768f4ac81cf1d439956bc1005b8dd156
jomon111 <yuku.uehara@gmail.com>
2024-02-17 15:43:00 +0900

    css

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ function toggleMenu() {
     openButton.classList.toggle('active');
 }
 
+function btnAlert(){
+    alert('サービス開始までもう少々お待ちください。\n下記フォームよりお問合せいただいた方から、順次ご連絡いたします。')
+}
+
 // Get the openbtn2 element and attach the event listener
 const openButton = document.querySelector('.openbtn2');
 openButton.addEventListener('click', toggleMenu);
